403b131427239d723193d4d87d11661ad29b5b03
aldirif <97742642+AldiRif@users.noreply.github.com>
2023-01-09 13:31:52 +0700

    Add project files.

---
+++ dotnet-ef-core/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotnetEfCore.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<CategoryEntity> categories { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=ef_db;uid=root;pwd=Agustus97;");
+        }
+    }
+}
